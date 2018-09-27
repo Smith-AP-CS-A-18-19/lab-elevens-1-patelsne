fa7a16a152620e26bcf969f108c744d7cfa03e20
Sneha Patel <Supersneha@patelsne-19-sp3.accounts.berkeleyprep.org>
2018-09-27 11:28:16 -0400

    Here is my 11s 1 lab

--- CardTester.java
+++ CardTester.java
@@ -9,5 +9,14 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("king", "hearts", 13);
+		Card card2 = new Card("queen", "hearts", 12);
+		Card card3 = new Card("ace", "spades", 1);
+		Card card4 = new Card("king", "hearts", 13);
+		System.out.println("Card1 is a " + card1.toString());
+		System.out.println("Card2 is a " + card2.toString());
+		System.out.println("Card3 is a " + card3.toString());
+		System.out.println(card1.matches(card2));
+		System.out.println(card1.matches(card4));
 	}
 }
